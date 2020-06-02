918d6c3088380c0075f5a969c10655054dfe7cfa
nitishjm <nmallava@sfu.ca>
2020-06-02 16:46:11 -0700

    added functionality to edit and delete previous posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView
+)
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('post/new/', BlogCreateView.as_view(), name='post_new')
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/modify/', BlogUpdateView.as_view(), name='post_modify'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -18,3 +19,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_modify.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')

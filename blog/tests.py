17ed26ef41382fbc0c7d8cd43504d7edea6c6024
nitishjm <nmallava@sfu.ca>
2020-06-02 17:07:56 -0700

    updated test cases

--- blog/tests.py
+++ blog/tests.py
@@ -30,14 +30,36 @@ class BlogTests(TestCase):
 
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'This is a test post')
 
     def test_post_detail_view(self):
-        response = self.client.get('/post/1')
-        no_response = self.client.get('/post/10000')
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(no_response.status_code, 404)
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/10000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
         self.assertTemplateUsed(response, 'post_detail.html')
         self.assertContains(response, 'This is a test post')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'new post',
+            'author': self.user,
+            'body': 'sample text'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "new post")
+        self.assertContains(response, "sample text")
+        self.assertEqual(str(self.user), 'TestUser')
+
+    def test_post_modify_view(self):
+        response = self.client.post(reverse('post_modify', args='1'), {
+            'title': "Updated title",
+            'body': "Updated body"
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)

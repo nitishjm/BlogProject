68260d8234b6815699d156f7ca803789878da069
nitishjm <nmallava@sfu.ca>
2020-06-06 00:21:57 -0700

    created user authentication (sign-in and sign-up)

--- blog/urls.py
+++ blog/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/modify/', BlogUpdateView.as_view(), name='post_modify'),
-    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete')
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Post
 
 
